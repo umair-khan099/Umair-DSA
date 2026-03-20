b448fdee543ee0fdd40071c8f62af5adcc223131
Umair khan <ukkhan5476@gmail.com>
2026-03-20 14:19:48 +0530

    fix:question-10

--- week-1/question-10.js
+++ week-1/question-10.js
@@ -1,8 +1,8 @@
 // Find surface area of sphere
 
 const surfaceAreaOfSphere = (radius) => {
-  let area = 4 * 3.14 * radius * radius;
-  return area;
+  let area = 4 * Math.PI * Math.pow(radius, 2);
+  return area.toFixed(2);
 };
 
 console.log(surfaceAreaOfSphere(7));
