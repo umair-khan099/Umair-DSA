e70796799b06128bb830437368732c6a7061324e
Umair khan <ukkhan5476@gmail.com>
2026-03-20 17:30:03 +0530

    feat: implement function to print "hello world" n times

---
+++ week-1/question-23.js
@@ -0,0 +1,18 @@
+// Accept an integer and Print \Hello World\" n times"
+
+// const printHello = (integer) => {
+//   let result = [];
+//   for (let i = 1; i <= integer; i++) {
+//     result.push("hello world");
+//   }
+//   return result;
+// };
+
+const printHello = (integer) => {
+  for (let i = 1; i <= integer; i++) {
+    console.log("hello world");
+  }
+};
+
+printHello(5);
+// console.log(printHello(5));
