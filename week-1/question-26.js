94e364fd4595281397ff8a37a217d1280b563020
Umair khan <ukkhan5476@gmail.com>
2026-03-20 17:47:13 +0530

    feat: add function to print multiplication table of a number

---
+++ week-1/question-26.js
@@ -0,0 +1,9 @@
+// Take a number as input and print its table (Ex: 5 * 1 = 5 ... up to 10 terms)
+
+const table = (num) => {
+  for (let i = 1; i <= 10; i++) {
+    console.log(5 * i);
+  }
+};
+
+table(5);
