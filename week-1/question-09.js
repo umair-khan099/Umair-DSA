2a77c2e7c1e7d74fce42c5a9c129b98f798cc512
Umair khan <ukkhan5476@gmail.com>
2026-03-20 11:08:11 +0530

    question 9 done

--- week-1/question-09.js
+++ week-1/question-09.js
@@ -1,1 +1,9 @@
 // Accept the three sides of triangle and calculate the area using Heron's formula
+
+const AreaofTriangle = (a, b, c) => {
+  let s = (a + b + c) / 2;
+  let areaoftreiangle = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+  console.log(areaoftreiangle);
+};
+
+AreaofTriangle(3, 4, 5);
