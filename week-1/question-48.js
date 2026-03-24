8e2205b6e4413772a6adce0ec1b1b1042f34a7fd
Umair khan <ukkhan5476@gmail.com>
2026-03-24 23:58:07 +0530

    feat: add inverted right triangle star pattern implementation

---
+++ week-1/question-48.js
@@ -0,0 +1,9 @@
+//  Inverted Right Triangle Pattern
+let rows = 5;
+for (let i = rows; i >= 1; i--) {
+  let pattern = "";
+  for (let j = 1; j <= i; j++) {
+    pattern += "*";
+  }
+  console.log(pattern);
+}
