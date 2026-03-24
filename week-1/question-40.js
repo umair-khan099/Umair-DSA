1a3c3ece4cb5db1e92f17de331e9052feeeb24c0
Umair khan <ukkhan5476@gmail.com>
2026-03-24 15:50:24 +0530

    feat: implement do-while loop to print "hello" until incorrect input

---
+++ week-1/question-40.js
@@ -0,0 +1,14 @@
+// Print \hello\" until user gives wrong input using do while"
+
+// let input;
+
+// do {
+//   console.log("hello");
+//   input = prompt("Type hello to continue");
+// } while (input == "hello");
+
+let input = "umair";
+
+do {
+  console.log("hello");
+} while (input == "hello");
