d5ee9d2ed2763dfabdacf7c5f32d033c00c002b9
Umair khan <ukkhan5476@gmail.com>
2026-03-28 10:24:39 +0530

    Array Reverse Without Using Extra space

---
+++ week-2/question-08.js
@@ -0,0 +1,17 @@
+// Array Reverse Without Using Extra space
+
+let array = [1, 2, 3, 4, 5 ,6];
+
+let start = 0;
+let end = array.length - 1;
+
+while (start < end) {
+  let temp = array[start];
+  array[start] = array[end];
+  array[end] = temp;
+
+  start++;
+  end--;
+}
+
+console.log(array);
