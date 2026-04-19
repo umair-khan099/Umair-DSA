e31717bc6de2c3a4f7c23673f22a192fe69f46ed
Umair khan <ukkhan5476@gmail.com>
2026-04-20 00:45:51 +0530

    3-08

---
+++ week-3/question-08.js
@@ -0,0 +1,18 @@
+// Check if the string is Palindromic or not
+
+const str = "madam"; // user input maan lo
+
+let isPalindrome = true;
+
+for (let i = 0; i < str.length / 2; i++) {
+  if (str[i] !== str[str.length - 1 - i]) {
+    isPalindrome = false;
+    break;
+  }
+}
+
+if (isPalindrome) {
+  console.log("Palindrome");
+} else {
+  console.log("Not Palindrome");
+}
