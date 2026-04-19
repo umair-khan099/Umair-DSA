a7cdfc09fdfcbd223fb667e3fd90eaa840e25615
Umair khan <ukkhan5476@gmail.com>
2026-04-20 01:09:12 +0530

    3-18

---
+++ week-3/question-18.js
@@ -0,0 +1,10 @@
+// Sort the words of the sentence
+const str = "banana apple mango orange"; // input
+
+let words = str.split(" ");
+
+words.sort();
+
+let result = words.join(" ");
+
+console.log(result);
