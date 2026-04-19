c41bdeb1e1f9d62afbe54729b5a30091f6f0ef5d
Umair khan <ukkhan5476@gmail.com>
2026-04-20 00:42:02 +0530

    3-06

---
+++ week-3/question-06.js
@@ -0,0 +1,7 @@
+// Accept a string from user and print its each character on a new line
+
+const str = "hello"; // user input maan lo sir
+
+for (let i = 0; i < str.length; i++) {
+    console.log(str[i]);
+}
