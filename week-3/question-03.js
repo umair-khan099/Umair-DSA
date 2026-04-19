60fe4d56f86d548da91feccd7a968b4c0f7629d7
Umair khan <ukkhan5476@gmail.com>
2026-04-20 00:33:33 +0530

    3-03

---
+++ week-3/question-03.js
@@ -0,0 +1,24 @@
+// Rotate Image
+
+function rotate(matrix) {
+  let n = matrix.length;
+
+  // Step 1: Transpose
+  for (let i = 0; i < n; i++) {
+    for (let j = i; j < n; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+
+  // Step 2: Reverse each row
+  for (let i = 0; i < n; i++) {
+    matrix[i].reverse();
+  }
+}
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+rotate(matrix);
+console.log(matrix);
