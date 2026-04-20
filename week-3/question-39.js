b46900c6b33871332eb7cfc137f06f24c10b56a1
Umair khan <ukkhan5476@gmail.com>
2026-04-21 01:34:56 +0530

    3-39

---
+++ week-3/question-39.js
@@ -0,0 +1,20 @@
+// Valid Mountain Array
+
+function validMountainArray(arr) {
+  let n = arr.length;
+  if (n < 3) return false;
+
+  let i = 0;
+
+  while (i + 1 < n && arr[i] < arr[i + 1]) {
+    i++;
+  }
+  if (i === 0 || i === n - 1) return false;
+  while (i + 1 < n && arr[i] > arr[i + 1]) {
+    i++;
+  }
+
+  return i === n - 1;
+}
+
+console.log(validMountainArray([0, 3, 2, 1]));
