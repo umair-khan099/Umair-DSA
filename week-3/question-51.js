0bec26cc6a8f0e7b16e16574b52ec4f76253586b
Umair khan <ukkhan5476@gmail.com>
2026-04-21 01:56:13 +0530

    3-51

---
+++ week-3/question-51.js
@@ -0,0 +1,29 @@
+// Merge Intervals
+
+function merge(intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  let result = [intervals[0]];
+
+  for (let i = 1; i < intervals.length; i++) {
+    let last = result[result.length - 1];
+    let current = intervals[i];
+
+    if (current[0] <= last[1]) {
+      last[1] = Math.max(last[1], current[1]);
+    } else {
+      result.push(current);
+    }
+  }
+
+  return result;
+}
+
+console.log(
+  merge([
+    [1, 3],
+    [2, 6],
+    [8, 10],
+    [15, 18],
+  ]),
+);
