95127da8b026b74d1e64ef84cbd757b393c9a75d
Umair khan <ukkhan5476@gmail.com>
2026-04-21 01:22:10 +0530

    3-31

--- week-3/question-31.js
+++ week-3/question-31.js
@@ -2,17 +2,17 @@
 function checkAlmostEquivalent(word1, word2) {
   let freq = new Array(26).fill(0);
 
-  // count word1
+
   for (let ch of word1) {
     freq[ch.charCodeAt(0) - 97]++;
   }
 
-  // subtract word2
+
   for (let ch of word2) {
     freq[ch.charCodeAt(0) - 97]--;
   }
 
-  // check difference
+
   for (let val of freq) {
     if (Math.abs(val) > 3) {
       return false;
